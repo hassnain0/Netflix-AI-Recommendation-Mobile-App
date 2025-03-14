e3ad55fe1c4f69f03aa7a4466c361bc03751f985
Hassnain Ali <113959400+hassnain0@users.noreply.github.com>
2025-03-15 03:10:25 +0500

    Recommendation Added

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,11 +1,13 @@
-import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
+import { View, Text, ActivityIndicator, StyleSheet, FlatList } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useLocalSearchParams } from "expo-router";
 import { supabase } from "@/src/lib/supabase";
+import MovieItem from "@/src/lib/components/MovieItem";
 
 const MovieDetails = () => {
   const { id } = useLocalSearchParams();
   const [movie, setMovie] = useState(null);
+  const [similarMovies, setSimilarMovies] = useState();
 
   useEffect(() => {
     const fetchMovie = async () => {
@@ -22,18 +24,39 @@ const MovieDetails = () => {
     fetchMovie();
   });
 
+  //Fetch Similar Movies Based on Embeddings
+  useEffect(() => {
+    console.log("Movies Embeddings")
+    if (!movie?.embedding)
+       
+      {return;}
+
+    const fetchSimilarMovies = async () => {
+      const { data } = await supabase.rpc("match_movies", {
+        query_embedding: movie.embedding,
+        match_threshold: 0.78, // Choose an appropriate threshold for your data
+        match_count: 5, // Choose the number of matches
+      });
+      setSimilarMovies(data);
+    };
+    fetchSimilarMovies();
+  }, [movie?.embedding]);
+
   if (!movie) {
     return <ActivityIndicator />;
   }
-
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{movie?.title}</Text>
       <Text style={styles.subtitle}>{movie?.tagline}</Text>
       <Text style={styles.overview}>{movie?.overview}</Text>
 
-
       <Text style={styles.similar}>Similar Movies</Text>
+
+      <FlatList
+      data={similarMovies}
+      renderItem={MovieItem}
+      />
     </View>
   );
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from "expo-router";
 
 import { ThemeProvider, DarkTheme } from "@react-navigation/native";
+import { StatusBar } from "react-native";
+import { theme } from "@/src/colors/theme";
 
 const RootLayout = () => {
   return (
     <ThemeProvider value={DarkTheme}>
+      <StatusBar backgroundColor={theme.colors.background}/>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="[id]" options={{ title: "Movie details" }} />
